68ec0437f3b75945cbc8bff3e916372a6c140d4d
unknown <1831887894@qq.com>
2020-06-23 20:24:22 +0800

    建权

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"http://localhost:9999"'
-  BASE_API: '"http://192.168.0.185:9999"'
-  // BASE_API: '"http://api.jsztgj.com"'
+  // BASE_API: '"http://192.168.0.185:9999"'
+  BASE_API: '"http://api.jsztgj.com"'
 })
